1c634454b22e3c85d281186e8fbe32ed3ecd26b9
qweri0p <71005650+qweri0p@users.noreply.github.com>
2022-04-17 19:09:52 +0200

    Added try/except to main py file

--- json-dl.py
+++ json-dl.py
@@ -3,14 +3,18 @@ import khinsider, json
 f = open('music.json')
 def dl(ost, fmt):
     khinsider.download(ost,ost,True,[fmt],True)
-data = json.load(f)
+try:
+    data = json.load(f)
+except:
+    print("Your music.json isn't written correctly.")
+    exit()
+for i in data["mp3"]:
+    dl(i, 'mp3')
+print("Downloaded all mp3 soundtracks")
 for i in data["flac"]:
     dl(i,"flac")
 print("Downloaded all flac soundtracks")
 for i in data["m4a"]:
     dl(i, 'm4a')
 print("Downloaded all m4a soundtracks")
-for i in data["mp3"]:
-    dl(i, 'mp3')
-print("Downloaded all mp3 soundtracks")
 print("Done")
